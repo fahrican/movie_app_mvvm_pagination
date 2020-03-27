92533e5333be1db38c2922520ea2990bcf681f02
Fahri MacBook <fahri-can@web.de>
2020-03-27 22:48:49 +0100

    Create the Retrofit client.

---
+++ app/src/main/java/com/example/movieappmvvmwithpagination/data/api/TheMovieDBClient.kt
@@ -0,0 +1,47 @@
+package com.example.movieappmvvmwithpagination.data.api
+
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+
+object TheMovieDBClient {
+
+    fun getClient(): ITheMovieDB {
+        val requestInterceptor = Interceptor { chain ->
+            // Interceptor takes only one argument which is a lambda function
+            val url: HttpUrl = chain.request()
+                .url()
+                .newBuilder()
+                .addQueryParameter("api_key", API_KEY)
+                .build()
+
+            val request: Request = chain.request()
+                .newBuilder()
+                .url(url)
+                .build()
+
+            return@Interceptor chain.proceed(request) // explicitly returns a value from with @ annotation.
+        }
+        val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+            .addInterceptor(requestInterceptor)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .build()
+
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(BASE_URL)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create(ITheMovieDB::class.java)
+    }
+}
+
+const val API_KEY = "ea98aed9deb11f46c3d6d521ee63a0ae"
+const val BASE_URL = "https://api.themoviedb.org/3/"
+const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342"
