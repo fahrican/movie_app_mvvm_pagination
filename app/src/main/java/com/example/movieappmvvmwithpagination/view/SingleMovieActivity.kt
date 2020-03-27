0e032faf0c9e15c1a3d74c9142783b4cf72bf751
Fahri MacBook <fahri-can@web.de>
2020-03-27 19:24:10 +0100

    Add new activity to display a single movie.

---
+++ app/src/main/java/com/example/movieappmvvmwithpagination/view/SingleMovieActivity.kt
@@ -0,0 +1,13 @@
+package com.example.movieappmvvmwithpagination.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movieappmvvmwithpagination.R
+
+class SingleMovieActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
