cad5d3869aafefc32d839085e802f8a4099bcf3b
Fahri MacBook <fahri-can@web.de>
2020-03-27 22:01:37 +0100

    Create first model.

---
+++ app/src/main/java/com/example/movieappmvvmwithpagination/data/vo/MovieDetails.kt
@@ -0,0 +1,22 @@
+package com.example.movieappmvvmwithpagination.data.vo
+
+import com.squareup.moshi.Json
+
+data class MovieDetails(
+    val budget: Int,
+    val id: Int,
+    val overview: String,
+    val popularity: Double,
+    @field:Json(name = "poster_path")
+    val posterPath: String,
+    @field:Json(name = "release_date")
+    val releaseDate: String,
+    val revenue: Int,
+    val runtime: Int,
+    val status: String,
+    val tagline: String,
+    val title: String,
+    val video: Boolean,
+    @field:Json(name = "vote_average")
+    val rating: Double
+)
