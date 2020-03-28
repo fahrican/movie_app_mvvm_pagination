a4c5d26fda8e0cefdad3bc8d48d577749fd3bcc3
Fahri MacBook <fahri-can@web.de>
2020-03-28 17:29:33 +0100

    Add second method to fetch popular movies.

--- app/src/main/java/com/example/movieappmvvmwithpagination/data/api/ITheMovieDB.kt
+++ app/src/main/java/com/example/movieappmvvmwithpagination/data/api/ITheMovieDB.kt
@@ -1,11 +1,16 @@
 package com.example.movieappmvvmwithpagination.data.api
 
 import com.example.movieappmvvmwithpagination.data.vo.MovieDetails
+import com.example.movieappmvvmwithpagination.data.vo.MovieResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ITheMovieDB {
+
+    @GET("movie/popular")
+    fun getPopularMovie(@Query("page") page: Int): Single<MovieResponse>
 
     @GET("movie/{movie_id}")
     fun getMovieDetails(@Path("movie_id") id: Int): Single<MovieDetails>
