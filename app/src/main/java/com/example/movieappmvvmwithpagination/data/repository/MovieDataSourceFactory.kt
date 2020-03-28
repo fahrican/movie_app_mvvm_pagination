1c8a962ff860610cee2b70b545521ae8b26c5c7a
Fahri MacBook <fahri-can@web.de>
2020-03-28 21:00:35 +0100

    Implement factory for movie data source.

---
+++ app/src/main/java/com/example/movieappmvvmwithpagination/data/repository/MovieDataSourceFactory.kt
@@ -0,0 +1,22 @@
+package com.example.movieappmvvmwithpagination.data.repository
+
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.DataSource
+import com.example.movieappmvvmwithpagination.data.api.ITheMovieDB
+import com.example.movieappmvvmwithpagination.data.vo.Movie
+import io.reactivex.disposables.CompositeDisposable
+
+
+class MovieDataSourceFactory(
+    private val apiService: ITheMovieDB,
+    private val compositeDisposable: CompositeDisposable
+) : DataSource.Factory<Int, Movie>() {
+
+    val movieLiveDataSource = MutableLiveData<MovieDataSource>()
+
+    override fun create(): DataSource<Int, Movie> {
+        val movieDataSource = MovieDataSource(apiService, compositeDisposable)
+        movieLiveDataSource.postValue(movieDataSource)
+        return movieDataSource
+    }
+}
