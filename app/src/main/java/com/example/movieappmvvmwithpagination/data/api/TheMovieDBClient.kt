a18446c893fc4877d422d183ba6f05b189226da0
Fahri MacBook <fahri-can@web.de>
2020-03-28 20:34:27 +0100

    Add two more constants for later use of pagination.

--- app/src/main/java/com/example/movieappmvvmwithpagination/data/api/TheMovieDBClient.kt
+++ app/src/main/java/com/example/movieappmvvmwithpagination/data/api/TheMovieDBClient.kt
@@ -45,3 +45,5 @@ object TheMovieDBClient {
 const val API_KEY = "ea98aed9deb11f46c3d6d521ee63a0ae"
 const val BASE_URL = "https://api.themoviedb.org/3/"
 const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342"
+const val FIRST_PAGE = 1
+const val POSTS_PER_PAGE = 20
