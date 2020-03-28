99c86836028a4139b70988a9c20a1a4469816ecf
Fahri MacBook <fahri-can@web.de>
2020-03-28 15:59:39 +0100

    Add redirect from button to SingleMovieActivity.

--- app/src/main/java/com/example/movieappmvvmwithpagination/view/MainActivity.kt
+++ app/src/main/java/com/example/movieappmvvmwithpagination/view/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.movieappmvvmwithpagination.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.movieappmvvmwithpagination.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        single_movie_btn.setOnClickListener {
+            val intent = Intent(this, SingleMovieActivity::class.java)
+            intent.putExtra("id", 419704)
+            startActivity(intent)
+        }
     }
 }
