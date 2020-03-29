3c97cae25267eca0bfe6a0c4f82ec16980def2a4
Fahri MacBook <fahri-can@web.de>
2020-03-29 19:13:21 +0200

    Use of new constants for NetworkState messages.

--- app/src/main/java/com/example/movieappmvvmwithpagination/data/status/NetworkSate.kt
+++ app/src/main/java/com/example/movieappmvvmwithpagination/data/status/NetworkSate.kt
@@ -1,5 +1,10 @@
 package com.example.movieappmvvmwithpagination.data.status
 
+import com.example.movieappmvvmwithpagination.data.constant.ERROR_TEXT
+import com.example.movieappmvvmwithpagination.data.constant.REACHED_END_TEXT
+import com.example.movieappmvvmwithpagination.data.constant.RUNNING_TEXT
+import com.example.movieappmvvmwithpagination.data.constant.SUCCESS_TEXT
+
 enum class Status {
     RUNNING,
     SUCCESS,
@@ -9,9 +14,9 @@ enum class Status {
 class NetworkState(val status: Status, val msg: String) {
 
     companion object {
-        val LOADED = NetworkState(Status.SUCCESS, "success") //todo: make constants
-        val LOADING = NetworkState(Status.RUNNING, "running")
-        val ERROR = NetworkState(Status.FAILED, "Something went wrong!")
-        val END_OF_LIST = NetworkState(Status.FAILED, "You have reached the end.")
+        val LOADED = NetworkState(Status.SUCCESS, SUCCESS_TEXT)
+        val LOADING = NetworkState(Status.RUNNING, RUNNING_TEXT)
+        val ERROR = NetworkState(Status.FAILED, ERROR_TEXT)
+        val END_OF_LIST = NetworkState(Status.FAILED, REACHED_END_TEXT)
     }
 }
