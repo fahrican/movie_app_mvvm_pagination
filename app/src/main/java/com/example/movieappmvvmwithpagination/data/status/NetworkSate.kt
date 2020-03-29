386464f7c169b1d83e762be7817099aa1520c239
Fahri MacBook <fahri-can@web.de>
2020-03-29 18:38:24 +0200

    Move class in separate package.

---
+++ app/src/main/java/com/example/movieappmvvmwithpagination/data/status/NetworkSate.kt
@@ -0,0 +1,40 @@
+package com.example.movieappmvvmwithpagination.data.status
+
+enum class Status {
+    RUNNING,
+    SUCCESS,
+    FAILED
+}
+
+class NetworkState(val status: Status, val msg: String) {
+
+    companion object { // todo: use joined assignment
+        val LOADED: NetworkState
+        val LOADING: NetworkState
+        val ERROR: NetworkState
+        val END_OF_LIST: NetworkState
+
+        init {
+            LOADED =
+                NetworkState(
+                    Status.SUCCESS,
+                    "success"
+                )
+            LOADING =
+                NetworkState(
+                    Status.RUNNING,
+                    "running"
+                )
+            ERROR =
+                NetworkState(
+                    Status.FAILED,
+                    "Something went wrong!"
+                )
+            END_OF_LIST =
+                NetworkState(
+                    Status.FAILED,
+                    "You have reached the end."
+                )
+        }
+    }
+}
