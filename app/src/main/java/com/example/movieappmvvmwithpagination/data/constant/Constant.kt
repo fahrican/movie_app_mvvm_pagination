e719b7e5ebf3fee0bd6e733745a3780486b73b68
Fahri MacBook <fahri-can@web.de>
2020-03-29 17:37:27 +0200

    Move constants for the project in separate class.

---
+++ app/src/main/java/com/example/movieappmvvmwithpagination/data/constant/Constant.kt
@@ -0,0 +1,8 @@
+package com.example.movieappmvvmwithpagination.data.constant
+
+
+const val BASE_URL = "https://api.themoviedb.org/3/"
+const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342"
+const val FIRST_PAGE = 1
+const val POSTS_PER_PAGE = 20
+const val API_KEY = "api_key"
