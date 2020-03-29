9d945591446e4e210abd79b869bbbbac3cdb6295
Fahri MacBook <fahri-can@web.de>
2020-03-29 18:44:13 +0200

    Get rid of redundant code.

--- app/src/main/java/com/example/movieappmvvmwithpagination/data/status/NetworkSate.kt
+++ app/src/main/java/com/example/movieappmvvmwithpagination/data/status/NetworkSate.kt
@@ -9,9 +9,9 @@ enum class Status {
 class NetworkState(val status: Status, val msg: String) {
 
     companion object {
-        val LOADED: NetworkState = NetworkState(Status.SUCCESS, "success")
-        val LOADING: NetworkState = NetworkState(Status.RUNNING, "running")
-        val ERROR: NetworkState = NetworkState(Status.FAILED, "Something went wrong!")
-        val END_OF_LIST: NetworkState = NetworkState(Status.FAILED, "You have reached the end.")
+        val LOADED = NetworkState(Status.SUCCESS, "success")
+        val LOADING = NetworkState(Status.RUNNING, "running")
+        val ERROR = NetworkState(Status.FAILED, "Something went wrong!")
+        val END_OF_LIST = NetworkState(Status.FAILED, "You have reached the end.")
     }
 }
