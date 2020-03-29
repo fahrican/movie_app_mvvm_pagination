01c41766ab499ecd648e637e1ef14d264bf22ebc
Fahri MacBook <fahri-can@web.de>
2020-03-29 17:50:50 +0200

    Add todo.

--- app/src/main/java/com/example/movieappmvvmwithpagination/viewmodel/SingleMovieViewModel.kt
+++ app/src/main/java/com/example/movieappmvvmwithpagination/viewmodel/SingleMovieViewModel.kt
@@ -18,7 +18,7 @@ class SingleMovieViewModel(
         movieRepository.fetchSingleMovieDetails(compositeDisposable, movieId)
     }
 
-    val networkSate: LiveData<NetworkState> by lazy {
+    val networkSate: LiveData<NetworkState> by lazy { // todo: rename property
         movieRepository.getMovieDetailsNetworkState()
     }
 
