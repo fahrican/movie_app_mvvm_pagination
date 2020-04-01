aa1691c38d584ca120919cc2170c4410b3cb8a39
Fahri MacBook <fahri-can@web.de>
2020-04-01 21:58:28 +0200

    Add provider methods to use in MainActivity.

--- app/src/main/java/com/example/movieappmvvmwithpagination/di/ApiModule.kt
+++ app/src/main/java/com/example/movieappmvvmwithpagination/di/ApiModule.kt
@@ -3,6 +3,8 @@ package com.example.movieappmvvmwithpagination.di
 import com.example.movieappmvvmwithpagination.data.api.ITheMovieDB
 import com.example.movieappmvvmwithpagination.data.api.TheMovieDBClient
 import com.example.movieappmvvmwithpagination.data.repository.MovieDetailsRepository
+import com.example.movieappmvvmwithpagination.data.repository.MoviePagedListRepository
+import com.example.movieappmvvmwithpagination.viewmodel.MainViewModelFactory
 import com.example.movieappmvvmwithpagination.viewmodel.SingleMovieViewModelFactory
 import dagger.Module
 import dagger.Provides
@@ -22,4 +24,10 @@ class ApiModule {
     fun provideSingleMovieViewModelFactory(movieDetailsRepo: MovieDetailsRepository): SingleMovieViewModelFactory {
         return SingleMovieViewModelFactory(movieDetailsRepo, 1)
     }
+
+    @Provides
+    fun provideMoviePagedListRepository(apiService: ITheMovieDB) = MoviePagedListRepository(apiService)
+
+    @Provides
+    fun provideMainViewModelFactory(moviePagedListRepo: MoviePagedListRepository) = MainViewModelFactory(moviePagedListRepo)
 }
