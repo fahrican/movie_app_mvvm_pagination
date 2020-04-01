e692e67b1cd9b5b03cc41d3a5c5b37677b9001d2
Fahri MacBook <fahri-can@web.de>
2020-04-01 21:42:37 +0200

    Finish implementation of module for dependency injection.

--- app/src/main/java/com/example/movieappmvvmwithpagination/di/ApiModule.kt
+++ app/src/main/java/com/example/movieappmvvmwithpagination/di/ApiModule.kt
@@ -1,9 +1,25 @@
 package com.example.movieappmvvmwithpagination.di
 
+import com.example.movieappmvvmwithpagination.data.api.ITheMovieDB
+import com.example.movieappmvvmwithpagination.data.api.TheMovieDBClient
+import com.example.movieappmvvmwithpagination.data.repository.MovieDetailsRepository
+import com.example.movieappmvvmwithpagination.viewmodel.SingleMovieViewModelFactory
 import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class ApiModule {
 
+    @Singleton
+    @Provides
+    fun provideApi(): ITheMovieDB = TheMovieDBClient.getClient()
 
+    @Provides
+    fun provideMovieDetailsRepository(apiService: ITheMovieDB) = MovieDetailsRepository(apiService)
+
+    @Provides
+    fun provideSingleMovieViewModelFactory(movieDetailsRepo: MovieDetailsRepository): SingleMovieViewModelFactory {
+        return SingleMovieViewModelFactory(movieDetailsRepo, 1)
+    }
 }
